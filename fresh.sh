510e9fbe51c1870f20efe54b71cf1cbfe61cbc78
thleo <danielle.leong@ourzora.com>
2025-01-21 19:15:22 -0500

    add to shell script at new machine

--- fresh.sh
+++ fresh.sh
@@ -9,3 +9,11 @@ brew install gh
 brew install pyenv
 brew install pyenv-virtualenv
 
+# dbt autocomplete
+curl https://raw.githubusercontent.com/fishtown-analytics/dbt-completion.bash/master/dbt-completion.bash > ~/.dbt-completion.bash
+echo 'source ~/.dbt-completion.bash' >> ~/.zshrc
+
+# configure git
+git config --global user.name "thleo"
+read -p "Enter your org email: " email
+git config --global user.email "$email"
